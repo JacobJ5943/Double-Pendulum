4de2266525a3240ffe0173c8c629ce9fa958cac3
Jacob Johnson <johnson.jacob5943@gmail.com>
2023-01-02 19:08:27 -0600

    incrase max steps per tick

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
 pub const MASS_MAX: f32 = 100.0;
 pub const MASS_GRAVITY: f32 = 20.0;
-pub const MAX_STEPS_PER_TICK: usize = 50;
+pub const MAX_STEPS_PER_TICK: usize = 200;
 pub const MAX_GRANULARITY: f32 = 0.1;
